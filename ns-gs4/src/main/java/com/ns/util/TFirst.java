a7328e41498d553bfcdb7014924a991797bdf685
niewj <dotjar@163.com>
2016-11-03 16:34:03 +0800

    first commit

---
+++ ns-gs4/src/main/java/com/ns/util/TFirst.java
@@ -0,0 +1,10 @@
+package com.ns.util;
+
+/**
+ * Created by nieweijun on 2016/11/3.
+ */
+public class TFirst {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
